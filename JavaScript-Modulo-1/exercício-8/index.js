ab1ff0dcef12259589b9bd6eab48bd0954b0f8f4
Jhony-wqs <jhony270288@gmail.com>
2023-09-28 17:45:19 -0300

    exercício

--- JavaScript-Modulo-1/exercício-8/index.js
+++ JavaScript-Modulo-1/exercício-8/index.js
@@ -1,30 +1,47 @@
-let quantidadeImoveis = ''
 let opcao = ''
 let adicionarImoveis = []
-  
+ 
 
 
 do{
     opcao = prompt(
         'Quantidade de Imóveis cadastrado :' +
-         quantidadeImoveis.length + 
+         adicionarImoveis.length + 
         '\n1. Adicionar Imóveis'+
         '\n2. Mostrar Imóveis'+
         '\n Sair')
         switch(opcao){
             case '1':
-             quantidadeImoveis +=1
-             for(let i = 0; i< quantidadeImoveis; i++){
-              let nome = prompt('Nome do proprietário ?')
-              let quartos = prompt('quantidade de quartos ?')
-              let banheiro = prompt('Qauntidade de banheiro ?')
-              let garagem = prompt("Tem garagem ?")
-             }
-
+            const imoveis = {}
+              imoveis.proprietario = prompt('Informe o nome do proprietário do Imóvel ?')
+              imoveis.quartos = prompt('Informe a quantidade de quartos ?')
+              imoveis.banheiro = prompt('Informe a quntidade de banheiro(s) ?')
+              imoveis.garagem = prompt('O imóvel tem garagem SIM/NÃO ?')  
+               const confirmacao = confirm('Favor verificar os dados se estão corretos ! \nSalvar este imóvel SIM/NÃO ?'+
+               '\n\nProprietário :' +imoveis.proprietario + 
+               '\nQuantiadade de quartos :' +imoveis.quartos +
+               '\nQuantidade de banheiro(s) :' + imoveis.banheiro +
+               '\nO imóvel tem garagem ? :'+imoveis.garagem)      
+                 
+               if(confirmacao){
+                adicionarImoveis.push(imoveis)
+                alert('Imóvel salvo com sucesso!! ')
+               }else{
+                alert('Voltando ao menu !!!')
+               }
+              
+            
               break
             case '2':
-              
-              
+              for(let i = 0 ; i < adicionarImoveis.length ; i++){
+                  alert('Imóvel :'+(i+1)+
+                     '\n\nProprietário :'+ adicionarImoveis[i].proprietario+
+                     '\nQuartos : '+adicionarImoveis[i].quartos+
+                     '\nBanheiros : '+adicionarImoveis[i].banheiro+
+                     '\nGaragem : '+adicionarImoveis[i].garagem
+                  )
+             
+              }
                break            
             case '3':
               alert('Saindo...')
@@ -35,3 +52,12 @@ do{
         }
 
 }while(opcao !== '3')
+
+
+
+
+
+
+
+
+            
