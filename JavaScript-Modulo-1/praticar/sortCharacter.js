f5a93fcba7d10b971596ee3767e01fe7eb6dba51
Jhony-wqs <jhony270288@gmail.com>
2023-11-05 10:55:36 -0300

    exercício

--- JavaScript-Modulo-1/praticar/sortCharacter.js
+++ JavaScript-Modulo-1/praticar/sortCharacter.js
@@ -3,7 +3,7 @@ const baseDeDados = require('./database')
 
 const ordered = baseDeDados.characters.sort((x,y)=> x.name.localeCompare(y.name))
 
-console.log(ordered)
+ console.log(ordered)
 
 
 const array = [1,2,3,4,5,6,7,8,13,14,9,10,11,12]
@@ -13,3 +13,14 @@ const ordenarNumerais = array.sort((a,b)=>{
 })
 
  console.log(ordenarNumerais)
+
+
+
+const alfabeto = ['b','c','a','d','f','e']
+
+  function alfaOrdenado(){
+   return alfabeto.sort()
+  }
+
+
+console.log(alfaOrdenado())
