ca6ff1857eae2b9d8bf6636c725c3ae508e81afd
Jhony-wqs <jhony270288@gmail.com>
2024-01-15 18:39:15 -0300

    exercício

--- JavaScript-Modulo-1/aula-DOM-3-criando-elemento/criando-elemento.js
+++ JavaScript-Modulo-1/aula-DOM-3-criando-elemento/criando-elemento.js
@@ -1,16 +1,16 @@
 function addInput(){
-    const ul = document.getElementById('inputs')
+    const ul = document.getElementById('inputs')//pegando a tag ul e colocando em uma variável.
 
-    const newLi = document.createElement('li')
-    newLi.className = 'list-item'
-    newLi.innerText = 'Texto do Input :'
+    const newLi = document.createElement('li')//criando uma tag <li></li> 
+    newLi.className = 'list-item'//colocando uma classe
+    newLi.innerText = 'Texto do Input :'//colocando o nome que vai no input
     
 
-    const newInput = document.createElement('input')
-    newInput.type = 'text'
-    newInput.id = 'input'
+    const newInput = document.createElement('input')//criando um input
+    newInput.type = 'text'//escolhendo o atributo do input
+    newInput.id = 'input'//colocando um id no input
     
 
-    newLi.appendChild(newInput)
-    ul.appendChild(newLi)
+    newLi.appendChild(newInput)//criando o input no html
+    ul.appendChild(newLi)//criando o li no html
 }
