24b56550c74c9373ec6f7125292640496539fb6a
Jhony Wuaanderson <jhony270288@gmail.com>
2025-04-06 11:31:04 -0300

    implementando condicionais

--- Git-Github/index.js
+++ Git-Github/index.js
@@ -1,11 +1,9 @@
-function main() {
-    console.log("Bem-vindo ao conversor de temperaturas!");
-}
-
-main();
-
 function celsiusToFahrenheit(celsius) {
     return (celsius * 9 / 5) + 32;
+}
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
 }
 
 function main() {
@@ -13,6 +11,10 @@ function main() {
     const tempC = parseFloat(prompt("Digite a temperatura em Celsius: "));
     const tempF = celsiusToFahrenheit(tempC);
     console.log(`${tempC}°C é igual a ${tempF}°F`);
+
+    const tempFInput = parseFloat(prompt("Digite a temperatura em Fahrenheit: "));
+    const tempCResult = fahrenheitToCelsius(tempFInput);
+    console.log(`${tempFInput}°F é igual a ${tempCResult}°C`);
 }
 
 main();
