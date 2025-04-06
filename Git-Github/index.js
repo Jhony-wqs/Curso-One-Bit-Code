55033b3b2db90cac3f345888151cb06637810b57
Jhony Wuaanderson <jhony270288@gmail.com>
2025-04-06 11:27:38 -0300

    adicionado a função que tranforma graus celcius em fahrenheit

--- Git-Github/index.js
+++ Git-Github/index.js
@@ -3,3 +3,16 @@ function main() {
 }
 
 main();
+
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function main() {
+    console.log("Bem-vindo ao conversor de temperaturas!");
+    const tempC = parseFloat(prompt("Digite a temperatura em Celsius: "));
+    const tempF = celsiusToFahrenheit(tempC);
+    console.log(`${tempC}°C é igual a ${tempF}°F`);
+}
+
+main();
