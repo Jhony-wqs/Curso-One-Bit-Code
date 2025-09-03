51805bd712c1c255cb75bff706b0baadb83460f1
Jhony Wuanderson De Queiroz Silva <jhony270288@gmail.com>
2025-09-03 11:46:17 -0300

    aulas sobre react

---
+++ React/aula-2-etendendo-jsx/index.jsx
@@ -0,0 +1,13 @@
+const element = <h1>Olá, Mundo! </h1>
+ const otherElement = (
+      <div>
+        <h1>Texto</h1>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </div>
+ )
---
+++ React/aula-3-componentes -react/index.jsx
@@ -0,0 +1,12 @@
+function PrimaryButton() {
+    return <button>Meu Botão</button>
+}
+
+function Conatiner () {
+    return (
+        <div className="container">
+            <h1>texto</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet expedita repudiandae vitae cumque? Quibusdam totam, nulla eos officia, molestiae harum molestias sint minima odit, quo quaerat velit porro beatae. Deleniti.</p>
+        </div>
+    )
+}
